c74d1729927dc86162fc8ccf91d39a25ef226804
CS-Labs <cseely@unm.edu>
2016-06-11 00:17:04 -0600

    Adding solution to problem one in C++.

---
+++ C++/Problem_One.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream>
+/*Author: Christian Seely*/
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i < 1000; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)sum += i;
+	}
+	std::cout << sum;
+
+    return 0;
+}
+
