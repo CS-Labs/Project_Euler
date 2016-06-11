222eab3808c5b822b8141771adc7d6b19b6a0118
CS-Labs <cseely@unm.edu>
2016-06-11 00:25:52 -0600

    Adding solution to problem three in C++.

---
+++ C++/Problem_3.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+int main()
+{
+	long max = 0;
+	long long n = 600851475143;
+	std::vector<long> primeFactors;
+	long div = 2;
+	bool terminate = false;
+	long remainder = 0;
+	//Fill primeFactors with prime factors of n. 
+	while (!terminate)
+	{
+		if ((div*div) > n)
+		{
+			if (n != 1)
+			{
+				primeFactors.push_back(n);
+				terminate = true;
+			}
+			else
+			{
+				terminate = true;
+			}
+		}
+		if (n%div == 0 && !terminate)
+		{
+			primeFactors.push_back(div);
+			n /= div;
+			if (remainder != 0)++div;
+			while (n%div == 0 && !terminate) n /= div;
+		}
+		++div;
+	}
+	//Find largest prime factor in prime factors.
+	for (long pf : primeFactors)
+	{
+		if (pf > max) max = pf;
+	}
+	std::cout << max;
+    return 0;
+}
+
