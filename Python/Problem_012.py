40e41190d932d45369d6d85ca4a32c2528bf87cb
CS-Labs <cseely@unm.edu>
2016-06-12 01:37:51 -0600

    Adding solution to problem twelve in Python.

---
+++ Python/Problem_012.py
@@ -0,0 +1,28 @@
+# Author: Christian Seely
+
+# TODO: Try to optimize program using the Tau function.
+import math
+# Function to find the number of factors for some
+# number num.
+def numFactors(num):
+    nFactors = 0
+    # We only have to check up to the square root of the num.
+    bound = int(math.sqrt(num)+1)
+    for div in range(1, bound):
+        if(num%div==0):
+            nFactors += 1
+    # Factors come in pairs multiple answer by two.
+    return (nFactors*2)
+
+
+def problemTwelve():
+    n = [1]
+    i = 2
+    while(True):
+        sum = int(i*(i+1)/2) # Obtain triangle number.
+        if(numFactors(sum)>500):
+            return sum
+        i+=1
+
+print(problemTwelve())
+
