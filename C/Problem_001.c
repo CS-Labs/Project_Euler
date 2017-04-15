567b1f2afb50e649b4527ad67faf21eb77d36ec0
Christian Seely <cseely@unm.edu>
2017-04-15 00:53:11 -0600

    Adding solution to first problem in C.

---
+++ C/Problem_001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void problemOne();
+
+void main(void) {
+    problemOne();
+}
+
+void problemOne()
+{
+    int sum = 0;
+    for(int i = 0; i < 1000; i++)
+    {
+        if(i%3==0 || i%5==0) sum+=i;
+    }
+
+    printf("%d",sum);
+
+}
