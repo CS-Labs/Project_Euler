d602b128e2453040a0f66f27a6a52660fa5280c0
CS-Labs <cseely@unm.edu>
2017-06-27 21:43:17 -0600

    updating.

--- Python/Problem_048.py
+++ Python/Problem_048.py
@@ -1,9 +1,7 @@
 # Author Christian Seely
 
 def problemFortyEight():
-    # Sum up the series 1^1 + 2^2 +... 1000^1000 then reverse
-    # the resulting number. Next take the first 10 numbers off and reverse the
-    # resulting 10 numbers to get the answer.
-    return str(sum([i**i for i in range(1,1001)]))[::-1][:10][::-1]
-
+    # Sum up the series 1^1 + 2^2 +... 1000^1000 then take
+    # the first 10 numbers. 
+    return str(sum([i**i for i in range(1,1001)]))[-10:]
 print(problemFortyEight())
