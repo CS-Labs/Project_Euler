9cc4d98d06e9127b2adfd9ec275beb2a89183ea6
CS-Labs <cseely@unm.edu>
2017-06-27 22:19:59 -0600

    Adding solution to problem 24

---
+++ Python/Problem_024.py
@@ -0,0 +1,11 @@
+# Author Christian Seely
+import itertools
+
+def problemTwentyFour():
+    # Generate all permutations in lexicographical order where each permutation
+    # is confined to a tuple. Next use islice to move the one millionths permutation
+    # to the front of the iterator. Then call next to grab the one millionth item from
+    # the iterator. Finally, consolidate the digits in the tuple into a string.
+    return  "".join(d for d in next(itertools.islice(itertools.permutations("0123456789"), 999999, None)))
+
+print(problemTwentyFour())
