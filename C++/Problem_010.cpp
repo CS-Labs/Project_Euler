65e9c3a102617510adf3d6c64a9387fd7241d730
CS-Labs <cseely@unm.edu>
2018-02-03 19:14:47 -0700

    Adding solution to probem 10 in c++.

---
+++ C++/Problem_010.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+
+/*Author: Christian Seely*/
+
+/*
+ Method to find the primes from 2 to 2,000,000
+ using the Sieve of Eratosthenes algorithm and then
+ sum them all together.
+*/
+long long problemTen()
+{
+	int scope = 2000000;
+	// The index in the array represents a natural number.
+    // The possible values at each index have the following meaning :
+	// 1 = Prime, 0 = Not prime.
+	std::vector<int> n(scope, 1);
+	int bound = std::sqrt(n.size());
+	int j, k;
+	for (int i = 2; i < bound; i++)
+	{
+		if (n[i] == 1)
+		{
+			j = (i*i);
+			k = 1;
+			while (j < scope)
+			{
+				n[j] = 0;
+				j = (i*i) + (k*i);
+				k += 1;
+			}
+		}
+	}
+	long long sum = 0;
+	// Sum all the primes. 
+	for (int i = 2; i < scope; i++)
+	{
+		if (n[i]) sum += i;
+	}
+	return sum;
+}
+
+
+int main()
+{
+	std::cout << problemTen() << std::endl;
+	return 0;
+}
+
