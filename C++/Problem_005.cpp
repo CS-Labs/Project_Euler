12ec15a29358a71d4f4e68b917f5657ee8aa9b86
CS-Labs <cseely@unm.edu>
2018-02-03 18:24:36 -0700

    Adding solution to problem 5 in c++.

---
+++ C++/Problem_005.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <math.h>
+#include <execution>
+
+/*Author: Christian Seely*/
+
+
+
+int problemFive()
+{
+	std::vector<int> range(20);
+	std::iota(range.begin(), range.end(), 1);
+	// Excecution method: parralel, start, end, initial acc, custom reduction lambda. 
+	return std::reduce(std::execution::par, range.begin(), range.end(), 1,[](int a, int b) {return a / std::gcd(a, b)*b;});
+}
+
+
+int main()
+{
+	std::cout << problemFive() << std::endl;
+	return 0;
+}
+
