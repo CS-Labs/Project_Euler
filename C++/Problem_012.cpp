92641b34ee109f64e2ddee398b39253917baea49
CS-Labs <cseely@unm.edu>
2018-02-03 23:41:24 -0700

    Adding solution to problem 12 in c++.

---
+++ C++/Problem_012.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+
+/*Author: Christian Seely*/
+
+long getNumFactors(int n)
+{
+	long nFactors = 0;
+	double bound = std::sqrt(n) + 1;
+	for (int i = 1; i < bound; i++)
+	{
+		if (n%i == 0) nFactors += 1;
+	}
+	return (nFactors * 2);
+}
+
+
+long problemTwelve()
+{
+	int i = 2;
+	int sum;
+	while (1)
+	{
+		sum = i * (i + 1) / 2; // Obtain triangle number.
+		if (getNumFactors(sum) > 500) return sum;
+		i++;
+	}
+
+}
+
+int main()
+{
+	std::cout << problemTwelve() << std::endl;
+	return 0;
+}
+
